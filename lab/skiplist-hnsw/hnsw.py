2320d548da2a3e3724fbd0173bb501515d3039ac
nanni00 <giovanni.malaguti00@gmail.com>
2025-09-22 18:25:56 +0200

    Skiplist-HNSW lab

---
+++ lab/skiplist-hnsw/hnsw.py
@@ -0,0 +1,308 @@
+import math
+import random
+import statistics
+from collections import defaultdict, deque
+from typing import Any, Callable, List, Optional, Set
+
+from tabulate import tabulate
+from tqdm import tqdm
+
+random.seed(130397)
+
+
+class GraphNode:
+    def __init__(self, item: Optional[Any], level: int = 0) -> None:
+        self.item: Optional[Any] = item
+        self.level: int = level
+
+        # In this way we do not consider a proper
+        # semantic for incoming/outgoing edges on the node
+        self.neighbors: List[Set[Any]] = [set() for _ in range(level + 1)]
+
+    def __hash__(self) -> int:
+        return hash(self.item)
+
+    def __str__(self) -> str:
+        return f"({self.item}@{self.level})"
+
+    def __repr__(self) -> str:
+        return f"({self.item}@{self.level})"
+
+
+def basic_distance(a: GraphNode, b: GraphNode):
+    if a.item is None or b.item is None:
+        return float("inf")
+    return abs(a.item - b.item)
+
+
+class HNSW:
+    def __init__(
+        self,
+        max_connections: int,
+        max_connections_lowest: int,
+        ef_construction: int,
+        normalization_factor: float,
+        distance: Callable = basic_distance,
+        verbose: bool = False,
+    ) -> None:
+        self.max_connections = max_connections
+        self.max_connections_lowest = max_connections_lowest
+        self.ef_construction = ef_construction
+        self.normalization_factor = normalization_factor
+        self.distance = distance
+        self.verbose = verbose
+
+        self.level = 0
+
+        # a different solution might involve to keep
+        # all the nodes in some data structure like a set
+        # or a dictionary of sets
+        #
+        # self._layers = defaultdict(set)
+
+        # entrance point
+        self.ep: Optional[GraphNode] = GraphNode(None, self.level)
+
+    def _random_level(self):
+        return math.floor(
+            -math.log(random.random(), math.e) * self.normalization_factor
+        )
+
+    def insert(self, q: Any):
+        W = set()
+
+        layer_i = self._random_level()
+        L = self.level
+        q = GraphNode(q, layer_i)
+
+        ep = {self.ep}
+
+        # first phase: descend down to the last layer
+        # where the new item is not present
+        for lc in range(L, min(L, layer_i), -1):
+            W = self.search_layer(q, ep, lc, ef=1)
+            ep = {self._nearest(q, W)}
+
+        # Now, we have to add the node on each layer
+        # to the lowest one
+        for lc in range(min(L, layer_i), -1, -1):
+            if self.ep.item is None:
+                continue
+
+            # select the number of connections
+            # typically at lowest level more connections are allowed
+            M = self.max_connections if lc > 0 else self.max_connections_lowest
+
+            W = self.search_layer(q, ep, lc, ef=self.ef_construction)
+
+            if not W:
+                continue
+
+            neighbors = self.select_neighbors_simple(q=q, candidates=W, k=M)
+            assert len(neighbors) <= M
+
+            # Update the connections, for the new node
+            # and for its neighbors too
+            q.neighbors[lc] = neighbors
+            for e in neighbors:
+                e.neighbors[lc].add(q)
+
+                # If now a node has more than ef_construction
+                # links, we have to shrink them
+                if len(e.neighbors[lc]) > M:
+                    e.neighbors[lc] = self.select_neighbors_simple(
+                        q=e, candidates=e.neighbors[lc], k=M
+                    )
+                    assert len(e.neighbors[lc]) <= M
+
+                    # It's possible that by shrinking this node's neighbors,
+                    # the new node we were trying to connect will be discarded.
+                    # In general, if the number of neighbors is large enough,
+                    # this won't be a problem and other nodes will link to it eventually
+                    #
+                    # assert q in e.neighbors[lc]
+
+            ep = W
+
+        if ep is None or layer_i > L:
+            self.level = layer_i
+            self.ep = q
+            if self.verbose:
+                print(self.ep.neighbors)
+                print(f"Update: ep-->{self.ep}, current level-->{self.level}")
+
+    def select_neighbors_simple(
+        self, q: GraphNode, candidates: Set[GraphNode], k: int, *args
+    ) -> Set[GraphNode]:
+        """
+        The simplest selection method
+        """
+        candidates = {c for c in candidates if c.item != q.item}
+
+        return set(sorted(candidates, key=lambda c: self.distance(q, c))[:k])
+
+    def search_layer(self, q: GraphNode, ep: Set[GraphNode], level: int, ef: int):
+        """
+        Searches for nearest neighboors to the query on the specified layer.
+        """
+        visited: Set[GraphNode] = set(ep)
+        candidates: Set[GraphNode] = set(ep)
+        nearest_n: Set[GraphNode] = set(ep)
+
+        while candidates:
+            nearest = self._nearest(q, candidates)
+            candidates.remove(nearest)
+
+            furthest = self._furthest(q, nearest_n)
+
+            if self.distance(q, nearest) > self.distance(q, furthest):
+                break
+
+            for e in nearest.neighbors[level]:
+                if e not in visited:
+                    visited.add(e)
+
+                    furthest = self._furthest(q, nearest_n)
+
+                    if (
+                        self.distance(q, e) < self.distance(q, furthest)
+                        or len(nearest_n) < ef
+                    ):
+                        candidates.add(e)
+                        nearest_n.add(e)
+
+                        if len(nearest_n) > ef:
+                            nearest_n.remove(self._furthest(q, nearest_n))
+
+        return nearest_n
+
+    def knn(self, q: GraphNode, k: int, ef: int) -> List[Any]:
+        W = set()
+
+        ep = self.ep
+
+        for lc in range(self.level, 0, -1):
+            W = self.search_layer(q, {ep}, lc, ef=1)
+            ep = self._nearest(q, W)
+
+        W = self.search_layer(q, {ep}, level=0, ef=ef)
+        results = self.select_neighbors_simple(q, W, k)
+        results = [n.item for n in results]
+        return results
+
+    def traverse_hnsw_graph(self) -> List[GraphNode]:
+        """
+        Traverse the entire HNSW graph from a given entry point using BFS.
+        """
+        visited = set()
+        queue = deque([self.ep])
+        traversal_order = []
+
+        while queue:
+            current_node = queue.popleft()
+
+            # Skip if already visited
+            if current_node.item in visited:
+                continue
+
+            # Mark as visited and add to traversal order
+            visited.add(current_node.item)
+            traversal_order.append(current_node)
+
+            # Explore neighbors at all layers of the current node
+            for layer in range(len(current_node.neighbors)):
+                for neighbor in current_node.neighbors[layer]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+        return traversal_order
+
+    def _nearest(self, q: GraphNode, points: Set[GraphNode]) -> GraphNode:
+        return min(points, key=lambda w: self.distance(q, w))
+
+    def _furthest(self, q: GraphNode, points: Set[GraphNode]) -> GraphNode:
+        return max(points, key=lambda w: self.distance(q, w))
+
+
+def get_ground_truth(q: Any, nodes: List[Any], k):
+    nodes = [n for n in nodes if n != q]
+    return sorted(sorted(nodes, key=lambda x: abs(q - x))[:k])
+
+
+def recall(ground_truth: List[Any], results: List[Any], k: int):
+    return len(set(ground_truth) & set(results)) / k
+
+
+if __name__ == "__main__":
+    # max number of neighbors per layer
+    M = 10
+
+    # max number of neighbors in the lowest layer
+    M_0 = M * 2
+
+    # normalization factor
+    mL = 1 / math.log(M, math.e)
+
+    ef_construction = 16
+
+    hsnw = HNSW(
+        max_connections=M,
+        max_connections_lowest=M_0,
+        ef_construction=ef_construction,
+        normalization_factor=mL,
+        verbose=False,
+    )
+
+    MAX_RANGE = int(1e9)
+    INDEX_SIZE = int(1e3)
+    NUM_QUERIES = 100
+
+    all_nodes = []
+    for q in tqdm(
+        random.sample(range(0, MAX_RANGE), INDEX_SIZE),
+        desc="Insertion: ",
+        total=INDEX_SIZE,
+    ):
+        hsnw.insert(q)
+        all_nodes.append(q)
+
+    all_nodes.sort()
+
+    visited = hsnw.traverse_hnsw_graph()
+    print(f"#connected nodes: {len(visited)}")
+
+    # this will increase the number of nearest neighbors
+    # candidates considered during the search, as a multiple
+    # of the K parameter
+    ef_search = 16
+
+    k_values = [1, 3, 5, 10]
+    results = defaultdict(list)
+
+    for k in tqdm(k_values, desc="Query: "):
+        for q in tqdm(
+            random.sample(range(0, MAX_RANGE), NUM_QUERIES),
+            desc=f"K = {k}: ",
+            leave=False,
+            position=2,
+        ):
+            knn = hsnw.knn(GraphNode(q), k, ef=ef_search)
+            gt = get_ground_truth(q, all_nodes, k)
+
+            r = recall(gt, knn, k)
+
+            results[k].append(r)
+
+    stats = []
+
+    for k, scores in results.items():
+        mean = statistics.mean(scores)
+        stdev = statistics.stdev(scores)
+        median = statistics.median(scores)
+
+        stats.append((k, mean, stdev, median, min(scores), max(scores)))
+
+    print("Recall@K results")
+    print(
+        tabulate(stats, headers=["K", "mean", "stdev", "mean", "min", "max"]), end="\n"
+    )
---
+++ lab/skiplist-hnsw/skip_list.py
@@ -0,0 +1,140 @@
+import random
+from typing import List, Optional
+
+random.seed(130399)
+
+
+class SkipListNode:
+    def __init__(self, item: int, level: int = 0) -> None:
+        self.item = item
+        self.level = level
+        self.forward: List[Optional["SkipListNode"]] = [None] * (level + 1)
+
+    def __str__(self) -> str:
+        return "[" + f"{self.item}".rjust(2) + "]"
+
+
+class SkipList:
+    def __init__(self, p: float = 0.25, max_level: int = 4) -> None:
+        self.p = p
+        self.max_level = max_level
+        self.level = 0
+        self._header = SkipListNode(None, max_level)
+
+    def _random_level(self) -> int:
+        level = 0
+        while random.random() < self.p and level < self.max_level:
+            level += 1
+
+        return level
+
+    def _compare_keys(self, k1: int, k2: int) -> int:
+        if k1 is None or k2 is None:
+            return -1 if k1 is None else 1
+        return (k1 > k2) - (k1 < k2)
+
+    def search(self, q) -> Optional[int]:
+        current = self._header
+
+        # Starting from the highest level, we go down
+        # to the bottom on the same node (vertical move)
+        for level in range(self.max_level, -1, -1):
+            # current.forward[level] gives us the access to the
+            # next node at the same current level (horizontal move)
+            while (
+                current.forward[level] is not None
+                and self._compare_keys(current.forward[level].item, q) < 0
+            ):
+                current = current.forward[level]
+
+        current = current.forward[0]
+
+        if current is not None and self._compare_keys(current.item, q) == 0:
+            return current.item
+        return None
+
+    def insert(self, q: int) -> None:
+        current = self._header
+
+        # at each level we will need to update
+        # a node, and here we keep track of which
+        # nodes must be updated in the end
+        update = [None] * (self.max_level + 1)
+
+        # the current level is not set to self.max_level,
+        # since a less number of layers might be used at this stage
+        for level in range(self.level, -1, -1):
+            while (
+                current.forward[level] is not None
+                and self._compare_keys(current.forward[level].item, q) < 0
+            ):
+                current = current.forward[level]
+            update[level] = current
+
+        if current is not None and self._compare_keys(current.item, q) == 0:
+            return
+
+        new_level = self._random_level()
+        new_node = SkipListNode(q, new_level)
+
+        # if we added a new layer, we have to update the
+        # list level (e.g. if no node before was at layer 3,
+        # and this new one reaches 4, we have to update other
+        # nodes properly)
+        if new_level > self.level:
+            for level in range(self.level + 1, new_level + 1):
+                update[level] = self._header
+            self.level = new_level
+
+        # update the previous node:
+        # from the level 0 (lowest) to the new level, its
+        # successor is the new node now
+        for level in range(new_level + 1):
+            new_node.forward[level] = update[level].forward[level]
+            update[level].forward[level] = new_node
+
+    def display(self):
+        layers = []
+        current = self._header.forward[0]
+
+        # get all the low level items
+        while current is not None:
+            layers.append(
+                [str(current)] * (current.level + 1)
+                + [None] * (self.level - current.level - 1)
+            )
+
+            current = current.forward[0]
+
+        layers = list(zip(*layers))
+
+        for level in range(self.level - 1, -1, -1):
+            print(f"#{level}#--", end="")
+            if layers[level][0] is not None:
+                print(">", end="")
+            else:
+                print("-", end="")
+            for i, node in enumerate(layers[level]):
+                if node is not None:
+                    print(node, end="")
+                else:
+                    print("----", end="")
+
+                if i == len(layers[level]) - 1:
+                    print("-->[]", end="")
+                    continue
+
+                if layers[level][i + 1] is not None:
+                    print("-->", end="")
+                else:
+                    print("---", end="")
+            print()
+
+
+if __name__ == "__main__":
+    s = SkipList(p=0.75, max_level=5)
+
+    for _ in range(10):
+        s.insert(random.randrange(0, 100))
+
+    s.display()
